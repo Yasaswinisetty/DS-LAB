803ed8af81ce68c82c60c7b0aaafc70f0db2e0ef
Yasaswinisetty <93637066+Yasaswinisetty@users.noreply.github.com>
2021-11-23 11:03:03 +0530

    Create output sLL.c

---
+++ output sLL.c
@@ -0,0 +1,423 @@
+1-Create
+
+2-Insert begin
+
+3-Insert at end
+
+4-Insert at position
+
+5-Delete begin
+
+6-delete at end
+
+7- Delete at position
+
+8-Display
+
+9-Reverse display
+
+10-Search
+
+11-Sort
+
+12-Exit
+
+ 
+
+Enter your choice: 1
+
+Enter number of node: 4
+
+12
+
+44
+
+24
+
+65
+
+1-Create
+
+2-Insert begin
+
+3-Insert at end
+
+4-Insert at position
+
+5-Delete begin
+
+6-delete at end
+
+7- Delete at position
+
+8-Display
+
+9-Reverse display
+
+10-Search
+
+11-Sort
+
+12-Exit
+
+ 
+
+Enter your choice: 2
+
+89
+
+1-Create
+
+2-Insert begin
+
+3-Insert at end
+
+4-Insert at position
+
+5-Delete begin
+
+6-delete at end
+
+7- Delete at position
+
+8-Display
+
+9-Reverse display
+
+10-Search
+
+11-Sort
+
+12-Exit
+
+ 
+
+Enter your choice: 3
+
+90
+
+1-Create
+
+2-Insert begin
+
+3-Insert at end
+
+4-Insert at position
+
+5-Delete begin
+
+6-delete at end
+
+7- Delete at position
+
+8-Display
+
+9-Reverse display
+
+10-Search
+
+11-Sort
+
+12-Exit
+
+ 
+
+Enter your choice: 4
+
+enter position3
+
+ 
+
+Enter element567
+
+1-Create
+
+2-Insert begin
+
+3-Insert at end
+
+4-Insert at position
+
+5-Delete begin
+
+6-delete at end
+
+7- Delete at position
+
+8-Display
+
+9-Reverse display
+
+10-Search
+
+11-Sort
+
+12-Exit
+
+ 
+
+Enter your choice: 5
+
+ 
+
+Deleted element: 891-Create
+
+2-Insert begin
+
+3-Insert at end
+
+4-Insert at position
+
+5-Delete begin
+
+6-delete at end
+
+7- Delete at position
+
+8-Display
+
+9-Reverse display
+
+10-Search
+
+11-Sort
+
+12-Exit
+
+ 
+
+Enter your choice: 6
+
+ 
+
+Deleted element: 901-Create
+
+2-Insert begin
+
+3-Insert at end
+
+4-Insert at position
+
+5-Delete begin
+
+6-delete at end
+
+7- Delete at position
+
+8-Display
+
+9-Reverse display
+
+10-Search
+
+11-Sort
+
+12-Exit
+
+ 
+
+Enter your choice: 7
+
+2
+
+ 
+
+Delted element: 441-Create
+
+2-Insert begin
+
+3-Insert at end
+
+4-Insert at position
+
+5-Delete begin
+
+6-delete at end
+
+7- Delete at position
+
+8-Display
+
+9-Reverse display
+
+10-Search
+
+11-Sort
+
+12-Exit
+
+ 
+
+Enter your choice: 8
+
+ 
+
+567 12 24 65
+
+1-Create
+
+2-Insert begin
+
+3-Insert at end
+
+4-Insert at position
+
+5-Delete begin
+
+6-delete at end
+
+7- Delete at position
+
+8-Display
+
+9-Reverse display
+
+10-Search
+
+11-Sort
+
+12-Exit
+
+ 
+
+Enter your choice: 9
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+65
+
+24
+
+12
+
+567
+
+1-Create
+
+2-Insert begin
+
+3-Insert at end
+
+4-Insert at position
+
+5-Delete begin
+
+6-delete at end
+
+7- Delete at position
+
+8-Display
+
+9-Reverse display
+
+10-Search
+
+11-Sort
+
+12-Exit
+
+ 
+
+Enter your choice: 10
+
+12
+
+ 
+
+Element is not found1-Create
+
+2-Insert begin
+
+3-Insert at end
+
+4-Insert at position
+
+5-Delete begin
+
+6-delete at end
+
+7- Delete at position
+
+8-Display
+
+9-Reverse display
+
+10-Search
+
+11-Sort
+
+12-Exit
+
+ 
+
+Enter your choice: 11
+
+1-Create
+
+2-Insert begin
+
+3-Insert at end
+
+4-Insert at position
+
+5-Delete begin
+
+6-delete at end
+
+7- Delete at position
+
+8-Display
+
+9-Reverse display
+
+10-Search
+
+11-Sort
+
+12-Exit
+
+ 
+
+Enter your choice: 8
+
+ 
+
+12 24 65 567
+
+1-Create
+
+2-Insert begin
+
+3-Insert at end
+
+4-Insert at position
+
+5-Delete begin
+
+6-delete at end
+
+7- Delete at position
+
+8-Display
+
+9-Reverse display
+
+10-Search
+
+11-Sort
+
+12-Exit
+
+ 
+
+Enter your choice: 12
